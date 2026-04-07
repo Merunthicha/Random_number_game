fe36fd1084a3772c24e91f086f22884185426b47
Merunthicha <merunthicha@gmail.com>
2026-04-07 23:39:06 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Random Number Game
+
+A simple number guessing game made with Python.
+Try to guess the number between a range you choose!
+
+![Game Screenshot](screenshot.png)
+
+## Installation
+
+1. Clone the repo:
+```bash
+git clone https://github.com/Merunthicha/random-number-game.git
+```
+
+2. Run the game
+```bash
+python main.py
+```
+
+---
+
+## How to Play
+
+1. Choose the range of numbers.
+2. Guess the number until you find the correct one.
+3. Follow hints: Higher / Lower
+4. Have fun!
+
+
+---
+
+## Creator
+
+- Name: Merunthicha G. Chanpirom Wong
+- GitHub: [@Merunthicha](https://github.com/Merunthicha)  
+- Telegram: [@Merunthicha](https://t.me/Merunthicha)
+- YouTube: [@Merunthicha](https://www.youtube.com/@Merunthicha)
+
+## Version
+
+```bash
+Version 1.0
+-
+Version 1.1
+- Debug
+Version 1.2
+- No Debug
+Version 1.3
+- WTF AGAIN
+ ```
---
+++ main.py
@@ -0,0 +1,110 @@
+# Random Number Game
+# Github: @Merunthicha
+# Version: 1.0
+
+# == IMPORT ==
+import random as ran
+
+# == RANDOM GAME ==
+def random_game():
+    print("Enter range of numbers do you want to play (Ex. 1 and 100)")
+
+    while True:
+        start_range = input("Start range of number: ")
+        if not start_range.isdigit():
+            print("Please enter a valid number!")
+            continue
+        start_range = int(start_range)
+
+        last_range = input("Last range of number: ")
+        if not last_range.isdigit():
+            print("Please enter a valid number!")
+            continue
+        last_range = int(last_range)
+        
+        if start_range > last_range:
+            print("=" * 40)
+            print("Start must be less than end!")
+            continue
+
+        print("=" * 40)
+        break
+    
+    random_number = ran.randint(start_range, last_range)
+
+    print(f"Great! Now guess the number between {start_range} and {last_range}")
+    while True:
+        guess_number = input("Enter a number do you think: ")
+        if not guess_number.isdigit():
+            print("Please enter a valid number!")
+            continue
+        guess_number = int(guess_number)
+
+        if guess_number > random_number:
+            print("=" * 40)
+            print(f"Lower than {guess_number}. Try again.")
+            continue
+        elif guess_number < random_number:
+            print("=" * 40)
+            print(f"Higher than {guess_number}. Try again.")
+            continue
+        else:
+            print("=" * 40)
+            print(f"{guess_number} is Correct!")
+            
+        play_again = input("Play again? (y/n): ")
+        if play_again.lower() == "n":
+            break
+        elif play_again == "y":
+            random_game()
+        break
+
+# == CREATOR INFO ==
+def creator_info():
+    print("Name:      Merunthicha")
+    print("Github:   @Merunthicha")
+    print("Telegram: @Merunthicha")
+    print("Youtube:  @Merunthicha")
+    print("")
+    print("[R] Return")
+    print("[E] Exit")
+    while True:
+        answer = input("Enter the answer: ").upper()
+        print("=" * 40)
+        if answer == "R":
+            main()
+        elif answer == "E":
+            break
+        else:
+            print("Enter only R or E")
+            continue
+        break
+
+
+
+# == MAIN ==
+def main():
+    while True:
+        print("Welcome to Random Number Game (owo!)")
+        print("[1] Start Game")
+        print("[2] Creator")
+        print("[3] Exit")
+        answer = input("Enter a number: ")
+        print("=" * 40)
+
+        if answer.isdigit():
+            answer = int(answer)
+            if answer == 1:
+                random_game()
+            elif answer == 2:
+                creator_info()
+            elif answer == 3 :
+                print("Goodbye!")
+            break
+        else:
+            print("Please typing a number only 1 or 2")
+        break
+        
+# == RUN ==
+if __name__ == "__main__":
+    main()
